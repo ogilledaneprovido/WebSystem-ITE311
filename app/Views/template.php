4d52401d001d3befb3ebbb4d78d3d0a2e63e5e18
Ogille Dane Provido <ogilledaneprovidoo@gmail.com>
2025-08-07 01:16:45 +0800

    Added Bootstrap template and Home controller

---
+++ app/Views/template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ITE311</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>Welcome to ITE311 Bootstrap Template!</h1>
+    </div>
+</body>
+</html>
