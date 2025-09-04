08fb2e8e562c967371fde1572c3f46a8713686fb
Jenie13 <jeniferlabina515@gmail.com>
2025-09-04 14:05:39 +0800

    Added database migrations and seeders

---
+++ app/Database/Migrations/2025-09-04-055232_CreateCoursesTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '150',
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'created_at DATETIME DEFAULT CURRENT_TIMESTAMP',
+            'updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('courses');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('courses');
+    }
+}
---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'name'     => 'Dane',
+                'email'    => 'dane@example.com',
+                'role'     => 'admin',
+                'password' => password_hash('admin123', PASSWORD_DEFAULT),
+            ],
+            [
+                'name'     => 'Jim',
+                'email'    => 'jim@example.com',
+                'role'     => 'instructor',
+                'password' => password_hash('teach123', PASSWORD_DEFAULT),
+            ],
+            [
+                'name'     => 'Ogille',
+                'email'    => 'ogille@example.com',
+                'role'     => 'student',
+                'password' => password_hash('stud123', PASSWORD_DEFAULT),
+            ],
+        ];
+
+        // Insert multiple users
+        $this->db->table('users')->insertBatch($data);
+    }
+}
